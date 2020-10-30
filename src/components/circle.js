e0aa2ed66c5834bf6d604543d84d8e3e17f45dba
Ayush Kumar <68886542+Hi-Ayush@users.noreply.github.com>
2020-10-30 23:43:10 +0530

    changes

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,10 @@
 // import using require
+import React from "react";
 
 // declare class
-
+import Shape from "./shape";
+export default class Circle extends Shape.Component {
+  calculateArea() {}
+}
 
 // export class using module.exports
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,11 @@
 // declare class
 
 // export class using module.exports
+import React from "react";
+export default class Shape extends React.Component {
+  Constructor() {
+    this.color = "blue";
+  }
+  drawShape() {}
+  calculateArea() {}
+}
